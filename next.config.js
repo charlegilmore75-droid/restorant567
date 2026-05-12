1e8b0bd90857d95290d180d268c400db433c8a11
charlegilmore75-droid <charlegilmore75@gmail.com>
2026-05-12 16:37:05 +0300

    Create next.config.js

---
+++ next.config.js
@@ -0,0 +1,20 @@
+const createNextIntlPlugin = require("next-intl/plugin");
+
+const withNextIntl = createNextIntlPlugin("./i18n/request.ts");
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      { protocol: "https", hostname: "" },
+      { protocol: "http", hostname: "" },
+    ],
+  },
+  experimental: {
+    serverActions: {
+      allowedOrigins: ["*"],
+    },
+  },
+};
+
+module.exports = withNextIntl(nextConfig);
